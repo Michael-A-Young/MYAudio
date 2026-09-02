02876bdf54b901505474e288e543ce671a7ce548
Michael Young <YoungM0202@outlook.com>
2026-09-02 23:04:30 +0100

    Define Debug/Release config preprocessor definitions.

--- include/MY/MY.h
+++ include/MY/MY.h
@@ -8,6 +8,14 @@ Summary:
 #ifndef MY_H
 #define MY_H
 
+#ifndef MY_DEBUG
+#define MY_DEBUG 0
+#endif
+
+#ifndef MY_RELEASE
+#define MY_RELEASE 0
+#endif
+
 #include "Compiler.h"
 #include "Platform.h"
 #include "Debug.h"
